c98764f77b159c754553f71e5980929a4b6ce2a6
sero-dappmaker <wingforgm2@gmail.com>
2020-07-28 18:09:32 +0800

    first commit

---
+++ precache-manifest.4cf969a1ead3b98636ab95cb1465757b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fcac62facf776a91548342024423fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc18422ae295d7f4ebc1",
+    "url": "./static/css/2.0615faac.chunk.css"
+  },
+  {
+    "revision": "83c32bc8a14b0e45220f",
+    "url": "./static/css/main.c616943e.chunk.css"
+  },
+  {
+    "revision": "cc18422ae295d7f4ebc1",
+    "url": "./static/js/2.cffbca8f.chunk.js"
+  },
+  {
+    "revision": "83c32bc8a14b0e45220f",
+    "url": "./static/js/main.6f45d52c.chunk.js"
+  },
+  {
+    "revision": "d61822e1c5124fabed77",
+    "url": "./static/js/runtime~main.d507e501.js"
+  },
+  {
+    "revision": "2d51db04f5cb3351c40beee8a3839c3c",
+    "url": "./static/media/head.2d51db04.png"
+  }
+]);
